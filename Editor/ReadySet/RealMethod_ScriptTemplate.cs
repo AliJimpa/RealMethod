393ddfeab564266b605bb8b4b16d565b20754cf4
Alijimpa <alibadpa007@gmail.com>
2025-05-27 14:24:53 +0330

    Create Script Template from Resource Folder

--- Editor/ReadySet/RealMethod_ScriptTemplate.cs
+++ Editor/ReadySet/RealMethod_ScriptTemplate.cs
@@ -7,82 +7,82 @@ namespace RealMethod
         [MenuItem("Assets/Create/Scripting/RealMethod/Asset/DataAsset", false, 80)]
         public static void CreateDataAsset()
         {
-            string Path = ScriptCreator.Create("DataAssetTemplate.txt", "MyDataAsset.cs", true);
+            string Path = ScriptCreator.Create("DataAssetTemplate.txt", "MyDataAsset.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Manager/DataManager", false, 80)]
         public static void CreateDataManager()
         {
-            string Path = ScriptCreator.Create("DataManagerTemplate.txt", "MyDataManager.cs", true);
+            string Path = ScriptCreator.Create("DataManagerTemplate.txt", "MyDataManager.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Service/GameService", false, 80)]
         public static void CreateGameServiceClass()
         {
-            string Path = ScriptCreator.Create("GameServiceTemplate.txt", "MyGameService.cs", true);
+            string Path = ScriptCreator.Create("GameServiceTemplate.txt", "MyGameService.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Essential/Game", false, 80)]
         public static void CreateGameClass()
         {
-            string Path = ScriptCreator.Create("GameTemplate.txt", "MyGame.cs", true);
+            string Path = ScriptCreator.Create("GameTemplate.txt", "MyGame.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Manager/GizmoManager", false, 80)]
         public static void CreateGizmoManager()
         {
-            string Path = ScriptCreator.Create("GizmoManagerTemplate.txt", "MyGizmoManager.cs", true);
+            string Path = ScriptCreator.Create("GizmoManagerTemplate.txt", "MyGizmoManager.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Essential/Manager", false, 80)]
         public static void CreateManager()
         {
-            string Path = ScriptCreator.Create("ManagerTemplate.txt", "MyManager.cs", true);
+            string Path = ScriptCreator.Create("ManagerTemplate.txt", "MyManager.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Manager/MixerManager", false, 80)]
         public static void CreateMixer()
         {
-            string Path = ScriptCreator.Create("MixerManagerTemplate.txt", "MyMixerManager.cs", true);
+            string Path = ScriptCreator.Create("MixerManagerTemplate.txt", "MyMixerManager.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Asset/PoolAsset", false, 80)]
         public static void CreatePoolAsset()
         {
-            string Path = ScriptCreator.Create("PoolAssetTemplate.txt", "MyPoolAsset.cs", true);
+            string Path = ScriptCreator.Create("PoolAssetTemplate.txt", "MyPoolAsset.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Service/RuleService", false, 80)]
         public static void CreateRuleService()
         {
-            string Path = ScriptCreator.Create("RuleServiceTemplate.txt", "MyRuleService.cs", true);
+            string Path = ScriptCreator.Create("RuleServiceTemplate.txt", "MyRuleService.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Essential/Service", false, 80)]
         public static void CreateService()
         {
-            string Path = ScriptCreator.Create("ServiceTemplate.txt", "MyServicec.cs", true);
+            string Path = ScriptCreator.Create("ServiceTemplate.txt", "MyServicec.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Setting/SettingSection", false, 80)]
         public static void CreateSettingSection()
         {
-            string Path = ScriptCreator.Create("SettingSectionTemplate.txt", "MySection.cs", true);
+            string Path = ScriptCreator.Create("SettingSectionTemplate.txt", "MySection.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Asset/SharedRootAsset", false, 80)]
         public static void CreateSharedRootAsset()
         {
-            string Path = ScriptCreator.Create("SharedRootAssetTemplate.txt", "MySharedRootAsset.cs", true);
+            string Path = ScriptCreator.Create("SharedRootAssetTemplate.txt", "MySharedRootAsset.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Service/StateService", false, 80)]
         public static void CreateStateService()
         {
-            string Path = ScriptCreator.Create("StateServiceTemplate.txt", "MyStateService.cs", true);
+            string Path = ScriptCreator.Create("StateServiceTemplate.txt", "MyStateService.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Asset/TableAsset", false, 80)]
         public static void CreateTableAsset()
         {
-            string Path = ScriptCreator.Create("TableTemplate.txt", "MyTableAsset.cs", true);
+            string Path = ScriptCreator.Create("TableTemplate.txt", "MyTableAsset.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Manager/UIManager", false, 80)]
         public static void CreateUIManager()
         {
-            string Path = ScriptCreator.Create("UIManagerTemplate.txt", "MyUIManager.cs", true);
+            string Path = ScriptCreator.Create("UIManagerTemplate.txt", "MyUIManager.cs");
         }
         [MenuItem("Assets/Create/Scripting/RealMethod/Essential/World", false, 80)]
         public static void CreateWorld()
         {
-            string Path = ScriptCreator.Create("WorldTemplate.txt", "MyWorld.cs", true);
+            string Path = ScriptCreator.Create("WorldTemplate.txt", "MyWorld.cs");
         }
 
 
