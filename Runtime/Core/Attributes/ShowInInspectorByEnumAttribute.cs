86b1668ed59ac34170a2b9cf6f4f5549bd8f959e
Alijimpa <alibadpa007@gmail.com>
2025-05-30 12:59:28 +0330

    Fix Editor Issue for Attribute Class

--- Runtime/Core/Attributes/ShowInInspectorByEnumAttribute.cs
+++ Runtime/Core/Attributes/ShowInInspectorByEnumAttribute.cs
@@ -1,6 +1,8 @@
 using System;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
 public class ShowInInspectorByEnumAttribute : PropertyAttribute
