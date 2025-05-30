f0e5216af07598a104aac84e9cbfd08dc02d2ea1
Alijimpa <alibadpa007@gmail.com>
2025-05-30 12:45:58 +0330

    Fix Editor Assmbly file

--- Tests/Inspector/InspectorGuideSection.cs
+++ Tests/Inspector/InspectorGuideSection.cs
@@ -1,34 +1,34 @@
 using UnityEditor;
 using UnityEngine;
 
-namespace RealMethod
-{
-    public class InspectorGuideSection : ProjectSettingSection
-    {
-        private SerializedObject projectSettings;
+// namespace RealMethod
+// {
+//     public class InspectorGuideSection : ProjectSettingSection
+//     {
+//         private SerializedObject projectSettings;
 
 
-        protected override void Initialized()
-        {
-        }
-        protected override void FirstSelected(ProjectSettingAsset Storage)
-        {
-            projectSettings = new SerializedObject(Storage);
-        }
-        protected override void Draw()
-        {
-            if (projectSettings == null) return;
-            EditorGUILayout.PropertyField(projectSettings.FindProperty("ShowHideAbblity"), new GUIContent("Show Inside Abblity"));
-            projectSettings.ApplyModifiedProperties();
-        }
-        protected override void Fix(int Id)
-        {
-        }
-        protected override string GetTitle()
-        {
-            return "Inspector";
-        }
+//         protected override void Initialized()
+//         {
+//         }
+//         protected override void FirstSelected(ProjectSettingAsset Storage)
+//         {
+//             projectSettings = new SerializedObject(Storage);
+//         }
+//         protected override void Draw()
+//         {
+//             if (projectSettings == null) return;
+//             EditorGUILayout.PropertyField(projectSettings.FindProperty("ShowHideAbblity"), new GUIContent("Show Inside Abblity"));
+//             projectSettings.ApplyModifiedProperties();
+//         }
+//         protected override void Fix(int Id)
+//         {
+//         }
+//         protected override string GetTitle()
+//         {
+//             return "Inspector";
+//         }
 
 
-    }
-}
+//     }
+// }
