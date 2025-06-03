76bf3afe1fa218c332af244c2caa55f48e6a34b8
Alijimpa <alibadpa007@gmail.com>
2025-06-03 15:41:42 +0330

    Add Some comment for Help

--- Runtime/Core/Definitions/ConfigAsset.cs
+++ Runtime/Core/Definitions/ConfigAsset.cs
@@ -2,6 +2,7 @@ namespace RealMethod
 {
     public abstract class ConfigAsset : DataAsset
     {
-
+        // You Can Just Write Variable & Readlonly 
+        // This class is a DataContainer Not have Function
     }
 }
