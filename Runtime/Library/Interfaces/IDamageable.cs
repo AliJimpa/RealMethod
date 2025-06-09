5f6d8733853bcf47fd3f04cd6dbc1774c59d5fdd
Alijimpa <alibadpa007@gmail.com>
2025-06-09 14:50:31 +0330

    Add Comment for IDamage

--- Runtime/Library/Interfaces/IDamageable.cs
+++ Runtime/Library/Interfaces/IDamageable.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 
 namespace RealMethod
 {
-    public interface IDamage<T> where T : struct
+    /// <summary>
+    /// Interface for objects that can take and restore damage.
+    /// </summary>
+    /// <typeparam name="D">The type representing damage data (e.g., int, float, struct).</typeparam>
+    public interface IDamage<D> where D : struct
     {
         /// <summary>
         /// Applies damage to the object.
         /// </summary>
         /// <param name="data">The raw data of damage.</param>
         /// <param name="source">The GameObject or system causing the damage.</param>
-        void TakeDamage(T data, GameObject source = null);
+        void TakeDamage(D data, GameObject source = null);
 
         /// <summary>
         /// Called when the object dies or is destroyed due to damage.
@@ -19,7 +23,7 @@ namespace RealMethod
         /// <summary>
         /// Restore the structure by a certain data.
         /// </summary>
-        void Restore(T data);
+        void Restore(D data);
 
         /// <summary>
         /// Returns true if the object is already dead.
@@ -27,13 +31,16 @@ namespace RealMethod
         bool IsDead { get; }
     }
 
-    public interface IDamageable<T,J> : IDamage<T> where T : struct
+    /// <summary>
+    /// Extended interface for damageable objects with health tracking.
+    /// </summary>
+    /// <typeparam name="D">The type representing damage data.</typeparam>
+    /// <typeparam name="H">The type representing health values (e.g., int, float).</typeparam>
+    public interface IDamageable<D, H> : IDamage<D> where D : struct
     {
-        J MaxHealth { get; }
-        J CurrentHealth { get; }
+        H MaxHealth { get; }
+        H CurrentHealth { get; }
 
-        public delegate void OnHealthChangedDelegate(J amount);
+        public delegate void OnHealthChangedDelegate(H amount);
     }
-
-
 }
