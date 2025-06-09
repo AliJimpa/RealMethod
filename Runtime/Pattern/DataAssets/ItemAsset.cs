0d058de2e22d0e4905909e9fbe5c2e9f82133514
Alijimpa <alibadpa007@gmail.com>
2025-06-09 19:35:55 +0330

    Refine ItemAsset : Add CreateSprite

--- Runtime/Pattern/DataAssets/ItemAsset.cs
+++ Runtime/Pattern/DataAssets/ItemAsset.cs
@@ -11,5 +11,23 @@ namespace RealMethod
         [SerializeField]
         protected Texture2D _icon;
         public Texture2D Icon => _icon;
+
+        // Functions
+        public virtual Sprite GetSpriteIcon()
+        {
+            if (_icon != null)
+            {
+                return Sprite.Create(
+                    _icon,
+                    new Rect(0, 0, _icon.width, _icon.height),
+                    new Vector2(0.5f, 0.5f)
+                );
+            }
+            else
+            {
+                Debug.LogWarning("ItemAsset: Icon is not assigned for item '" + _name + "'.");
+                return null;
+            }
+        }
     }
 }
