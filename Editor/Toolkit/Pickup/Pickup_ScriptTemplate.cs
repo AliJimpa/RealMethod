b3661b5b54e5395afa4dd34107ff21cbcadf1d30
Alijimpa <alibadpa007@gmail.com>
2025-06-26 19:59:48 +0330

    Refine Pickup Template

--- Editor/Toolkit/Pickup/Pickup_ScriptTemplate.cs
+++ Editor/Toolkit/Pickup/Pickup_ScriptTemplate.cs
@@ -9,12 +9,12 @@ namespace RealMethod.Editor
         {
             string Path = ScriptCreator.Create("PickupComponentTemplate.txt", "MyCustomPickup.cs");
         }
-        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/Pickup/Pickup2D", false, 80)]
+        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/Pickup/Pickup3D", false, 80)]
         public static void CreatePickup3DCollider()
         {
             string Path = ScriptCreator.Create("Pickup3DTemplate.txt", "MyPicku3D.cs");
         }
-        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/Pickup/Pickup3D", false, 80)]
+        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/Pickup/Pickup2D", false, 80)]
         public static void CreatePickup2DCollider()
         {
             string Path = ScriptCreator.Create("Pickup2DTemplate.txt", "MyPicku2D.cs");
