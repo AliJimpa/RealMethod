4ccf15321135a151b4b4cd581b9a78b91425fd63
Alijimpa <alibadpa007@gmail.com>
2025-06-26 20:33:27 +0330

    Implement ScriptTemplate for Ability Component

--- Editor/Toolkit/Ability/Ability_ScriptTemplate.cs
+++ Editor/Toolkit/Ability/Ability_ScriptTemplate.cs
@@ -4,6 +4,11 @@ namespace RealMethod.Editor
 {
     class Ability_ScriptTemplate
     {
+        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/Ability/Ability", false, 80)]
+        public static void CreateAbilityCommand()
+        {
+            string Path = ScriptCreator.Create("AbilityComponentTemplater.txt", "MyAbility.cs");
+        }
         [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/Ability/Power", false, 80)]
         public static void CreatePowerCommand()
         {
