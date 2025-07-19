87cfbe97671b7c45b76769d5e651d33687793191
Alijimpa <alibadpa007@gmail.com>
2025-07-20 01:44:11 +0330

    Implement First Verison of Effect

---
+++ Runtime/ReadySet/Components/Effect.cs
@@ -0,0 +1,131 @@
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace RealMethod
+{
+    [AddComponentMenu("RealMethod/General/Effect")]
+    public sealed class Effect : MonoBehaviour
+    {
+        [Header("Effect")]
+        [SerializeField]
+        private bool PlayOnStart = false;
+
+        private ParticleSystem[] AllParticles;
+        private AudioSource[] AllAudioSources;
+        private VisualEffect[] AllVisualEffects;
+        private ICommandExecuter[] AllExecutCommands;
+        private ICommandLife[] AllActionCommands;
+
+        /// Haptic
+        /// UI
+        /// Animation
+        /// AutoDestroy
+        /// Notify
+        /// CameraShake
+
+        // Unity Methods
+        private void Awake()
+        {
+            AllParticles = GetComponentsInChildren<ParticleSystem>();
+            foreach (var part in AllParticles)
+            {
+                if (part.isPlaying)
+                {
+                    Debug.LogWarning($"{part} autoplay is enable ");
+                }
+            }
+            AllAudioSources = GetComponentsInChildren<AudioSource>();
+            foreach (var aud in AllAudioSources)
+            {
+                if (aud.playOnAwake)
+                {
+                    Debug.LogWarning($"{aud} autoplay is enable ");
+                }
+            }
+            AllVisualEffects = GetComponentsInChildren<VisualEffect>();
+            AllExecutCommands = GetComponents<ICommandExecuter>();
+            ICommandInitiator[] CommandInitators = GetComponents<ICommandInitiator>();
+            foreach (var com in CommandInitators)
+            {
+                com.Initiate(this, this);
+            }
+        }
+        private void Start()
+        {
+            if (PlayOnStart)
+            {
+                Play();
+            }
+        }
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            foreach (var aud in GetComponentsInChildren<AudioSource>())
+            {
+                aud.playOnAwake = false;
+            }
+        }
+#endif
+
+        // Public Functions
+        public void Play()
+        {
+            foreach (var part in AllParticles)
+            {
+                part.Play();
+            }
+            foreach (var aud in AllAudioSources)
+            {
+                aud.Play();
+            }
+            foreach (var vef in AllVisualEffects)
+            {
+                vef.Play();
+            }
+            foreach (var com in AllExecutCommands)
+            {
+                com.ExecuteCommand(this);
+            }
+        }
+        public void Stop()
+        {
+            foreach (var part in AllParticles)
+            {
+                part.Pause();
+            }
+            foreach (var aud in AllAudioSources)
+            {
+                aud.Pause();
+            }
+            foreach (var vef in AllVisualEffects)
+            {
+                vef.pause = false;
+            }
+        }
+        public void Clear()
+        {
+            foreach (var part in AllParticles)
+            {
+                part.Stop(false, ParticleSystemStopBehavior.StopEmittingAndClear);
+            }
+            foreach (var aud in AllAudioSources)
+            {
+                aud.Stop();
+            }
+            foreach (var vef in AllVisualEffects)
+            {
+                vef.Reinit();
+            }
+        }
+
+    }
+
+
+    [System.Serializable]
+    public class EPrefab : Prefab<Effect>
+    {
+
+    }
+
+    
+}
