0fc70c46ffe630134432f90e30c62b4657f7b9b2
Alijimpa <alibadpa007@gmail.com>
2025-07-27 22:33:56 +0330

    Rename Enum Utility base

---
+++ Runtime/Library/SharedScripts/Enumeration/General_Enum.cs
@@ -0,0 +1,24 @@
+namespace RealMethod
+{
+    public enum UpdateMethod
+    {
+        None = 0,
+        LateUpdate = 2,
+        Update = 3,
+        FixedUpdate = 4
+
+    }
+    public enum LoadStage
+    {
+        Ascync,
+        Scync
+    }
+    public enum TriggerStage
+    {
+        None = 0,
+        Enter = 1,
+        Stay = 2,
+        Exit = 2,
+    }
+
+}
