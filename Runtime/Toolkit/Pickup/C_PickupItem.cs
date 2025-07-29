b579def79869bb74a0fbb567aac3d9e529cad541
Alijimpa <alibadpa007@gmail.com>
2025-07-29 18:23:19 +0330

    Implement Pickupable Command

---
+++ Runtime/Toolkit/Pickup/C_PickupItem.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+namespace RealMethod
+{
+    [AddComponentMenu("RealMethod/Commands/Execut/PickupItem")]
+    public sealed class C_PickupItem : ExecutCommand
+    {
+        [Header("Setting")]
+        [SerializeField]
+        private int quantity;
+        [SerializeField]
+        private bool AsNewIdentity = false;
+        [SerializeField, ConditionalHide("AsNewIdentity", true, false)]
+        private int capacity;
+        [Header("Resource")]
+        [SerializeField]
+        private ItemAsset items;
+
+        // ExecutCommand Methods
+        protected override bool OnInitiate(Object author, Object owner)
+        {
+            if (items == null)
+                Debug.LogWarning("Items is not valid");
+            return items != null;
+        }
+        protected override bool CanExecute(object Owner)
+        {
+            if (!enabled)
+                return false;
+
+            if (Owner is Component mono)
+            {
+                IPicker Picker = mono.GetComponent<IPicker>();
+                if (Picker != null)
+                {
+                    Trigger mytrigger = GetComponent<Trigger>();
+                    if (mytrigger)
+                    {
+                        return Picker.CanTake(mytrigger);
+                    }
+                    else
+                    {
+                        Debug.LogWarning("this class need Trigger behind self");
+                        return false;
+                    }
+
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Executer should be Component");
+                return false;
+            }
+        }
+        protected override void Execute(object Owner)
+        {
+            if (Owner is Component mono)
+            {
+                Inventory inven = mono.GetComponent<Inventory>();
+                if (inven)
+                {
+                    if (AsNewIdentity)
+                    {
+                        inven.AddNewItem(items, quantity, capacity);
+                    }
+                    else
+                    {
+                        inven.AddItem(items, quantity);
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning("Executer shoudl have Inventory Component");
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Executer should be Component");
+            }
+        }
+
+    }
+}
