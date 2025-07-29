c678be284088ea331239515c0ec81053c56eaea6
Alijimpa <alibadpa007@gmail.com>
2025-07-29 17:52:29 +0330

    Implement Trigger2D

---
+++ Runtime/ReadySet/Components/Physic/CommandTrigger2D.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace RealMethod
+{
+    [AddComponentMenu("RealMethod/Physic/Trigger2D")]
+    public sealed class CommandTrigger2D : Trigger2D
+    {
+        // Unity Methods
+        private void Awake()
+        {
+            foreach (var command in GetComponents<ICommandInitiator>())
+            {
+                command.Initiate(null, this);
+            }
+        }
+
+        // Trigger3D Methods
+        protected override void OnEnter(Collider2D other)
+        {
+            foreach (var command in GetComponents<ICommandExecuter>())
+            {
+                command.ExecuteCommand(other);
+            }
+        }
+        protected override void OnStay(Collider2D other)
+        {
+            foreach (var command in GetComponents<ICommandExecuter>())
+            {
+                command.ExecuteCommand(other);
+            }
+        }
+        protected override void OnExit(Collider2D other)
+        {
+            foreach (var command in GetComponents<ICommandExecuter>())
+            {
+                command.ExecuteCommand(other);
+            }
+        }
+    }
+}
