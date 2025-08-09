e1cee79ab0d52453db1a54bdd4ad1b36144cf61a
Alijimpa <alibadpa007@gmail.com>
2025-08-09 15:56:22 +0330

    Implement StatDifinition

--- Editor/Toolkit/RPG/RPG_ScriptTemplate.cs
+++ Editor/Toolkit/RPG/RPG_ScriptTemplate.cs
@@ -4,17 +4,22 @@ namespace RealMethod.Editor
 {
     class RPG_ScriptTemplate
     {
-        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/RPG/Profile", false, 80)]
+        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/RPG/StatDefinition", false, 80)]
+        public static void CreateStatDefinition()
+        {
+            string Path = RM_Create.Script("StatDefinitionTemplate.txt", "StatDefinition.cs");
+        }
+        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/RPG/StatProfile", false, 80)]
         public static void CreateStatProfile()
         {
             string Path = RM_Create.Script("StatProfileTemplate.txt", "MyProfile.cs");
         }
-        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/RPG/Buff", false, 80)]
+        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/RPG/StatBuff", false, 80)]
         public static void CreateBuffConfig()
         {
             string Path = RM_Create.Script("BuffConfigTemplate.txt", "MyBuff.cs");
         }
-        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/RPG/Resource", false, 80)]
+        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/RPG/ResourceData", false, 80)]
         public static void CreateResourceData()
         {
             string Path = RM_Create.Script("ResourceDataTemplate.txt", "MyResource.cs");
