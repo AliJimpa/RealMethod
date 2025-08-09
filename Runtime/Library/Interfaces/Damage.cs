763b9728ae2f0bc72d85dbaad969ed89252cd2d4
Alijimpa <alibadpa007@gmail.com>
2025-08-09 12:17:48 +0330

    Refine Damage Ineterface

--- Runtime/Library/Interfaces/Damage.cs
+++ Runtime/Library/Interfaces/Damage.cs
@@ -13,33 +13,29 @@ namespace RealMethod
         /// </summary>
         void Die();
     }
+    public interface IDamageable : IDamage
+    {
+        float MaxHealth { get; }
+        float CurrentHealth { get; }
+
+        /// <summary>
+        /// Simple damage (only amount + optional source)
+        /// </summary>
+        void TakeDamage(float amount, GameObject source = null);
+        void Restore(float amount);
+    }
     /// <summary>
     /// Interface for objects that can take and restore damage.
     /// </summary>
-    /// <typeparam name="TD">The type representing TakeDamage data (e.g., int, float, struct).</typeparam>
-    /// <typeparam name="R">The type representing Restore data (e.g., int, float, struct).</typeparam>
-    public interface IDamage<TD, R> : IDamage where TD : struct where R : struct
+    /// <typeparam name="T">The type representing TakeDamage data (e.g., int, float, struct).</typeparam>
+    public interface IDamageable<T, R> : IDamageable where T : struct where R : struct
     {
         /// <summary>
         /// Applies damage to the object.
         /// </summary>
         /// <param name="data">The raw data of damage.</param>
         /// <param name="source">The GameObject or system causing the damage.</param>
-        void TakeDamage(TD data, GameObject source = null);
-        /// <summary>
-        /// Restore the structure by a certain data.
-        /// </summary>
-        void Restore(R data);
-    }
-    /// <summary>
-    /// Extended interface for damageable objects with health tracking.
-    /// </summary>
-    /// <typeparam name="H">The type representing health values (e.g., int, float).</typeparam>
-    public interface IDamageable<TD, R, H> : IDamage<TD, R> where TD : struct where R : struct
-    {
-        H MaxHealth { get; }
-        H CurrentHealth { get; }
-
-        public delegate void OnHealthChangedDelegate(H amount);
+        void TakeDamage(T data);
+        void Restore(R amount);
     }
 }
