3d48f012829ab052eb706a3b832412f0da9ca1b9
Alijimpa <alibadpa007@gmail.com>
2025-09-01 15:36:12 +0330

    Create & Refine Tutorail Templates

--- Editor/Toolkit/Tutorial/Tutorial_ScriptTemplate.cs
+++ Editor/Toolkit/Tutorial/Tutorial_ScriptTemplate.cs
@@ -4,10 +4,16 @@ namespace RealMethod.Editor
 {
     class Tutorial_ScriptTemplate
     {
-        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/Tutorial/Message", false, 80)]
+        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/Tutorial/Screen", false, 80)]
+        public static void CreateTutorialScreen()
+        {
+            string Path = RM_Create.Script("TutorialScreenTemplate.txt.txt", "MyScreenTutorial.cs");
+        }
+
+        [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/Tutorial/UI", false, 80)]
         public static void CreateTutorialMessage()
         {
-            string Path = RM_Create.Script("TutorialMessageTemplate.txt.txt", "MyTutorialMessage.cs");
+            string Path = RM_Create.Script("TutorialUITemplate.txt.txt", "MyUITutorial.cs");
         }
 
         [MenuItem("Assets/Create/Scripting/RealMethod/Toolkit/Tutorial/Config", false, 80)]
