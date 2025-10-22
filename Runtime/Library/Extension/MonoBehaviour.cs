a0431788d30d8490fcd29bec8bb2ae11f34b2fe0
Alijimpa <alibadpa007@gmail.com>
2025-10-22 12:53:30 +0330

    Rename Function OnSpawn

--- Runtime/Library/Extension/MonoBehaviour.cs
+++ Runtime/Library/Extension/MonoBehaviour.cs
@@ -4,7 +4,7 @@ namespace RealMethod
 {
     public static class MonoBehaviour_Extension
     {
-        public static void SpawnEvent(this MonoBehaviour owner, Object spawner)
+        public static void Event_Spawn(this MonoBehaviour owner, Object spawner)
         {
             owner.SendMessage("OnSpawn", spawner, SendMessageOptions.DontRequireReceiver);
         }
