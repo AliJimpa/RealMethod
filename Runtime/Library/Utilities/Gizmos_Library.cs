f8525ff67e2da307cb28829c198df9494550d4e0
Alijimpa <alibadpa007@gmail.com>
2026-03-16 11:05:52 +0330

    Fix all Build Error code

--- Runtime/Library/Utilities/Gizmos_Library.cs
+++ Runtime/Library/Utilities/Gizmos_Library.cs
@@ -84,6 +84,7 @@ namespace RealMethod
             // Restore previous color
             Gizmos.color = prevColor;
         }
+#if UNITY_EDITOR
         // Draw a Text using Gizmos (With Colort)
         public static void Text(string text, Vector3 position, Color color, FontStyle font = FontStyle.Bold, TextAnchor ancher = TextAnchor.MiddleCenter)
         {
@@ -96,5 +97,6 @@ namespace RealMethod
             // Draw the label above the GameObject
             Handles.Label(position, text, style);
         }
+#endif
     }
 }
