a590b1014745bd7493cc138c85cb3b79fe5f03db
Alijimpa <alibadpa007@gmail.com>
2026-04-29 10:23:30 +0330

    Refine Basic Enums for Directions

--- Runtime/Library/SharedScripts/Enumeration/Directions.cs
+++ Runtime/Library/SharedScripts/Enumeration/Directions.cs
@@ -1,11 +1,58 @@
 namespace RealMethod
 {
-    public enum Corner
+    /// <summary>
+    /// Only the four cardinal directions.
+    /// </summary>
+    public enum Dir4
     {
-        UpRight,
-        UpLeft,
-        DownRight,
-        DownLeft
+        Up,
+        Right,
+        Down,
+        Left
     }
+    /// <summary>
+    /// Direction4 + Center (neutral).
+    /// </summary>
+    public enum Dir5
+    {
+        Center,
+        Up,
+        Right,
+        Down,
+        Left
+    }
+    /// <summary>
+    /// Cardinal + diagonal directions.
+    /// </summary>
+    public enum Dir8
+    {
+        Up,
+        UpRight,
+        Right,
+        DownRight,
+        Down,
+        DownLeft,
+        Left,
+        UpLeft
+    }
+    /// <summary>
+    /// Direction8 + Center.
+    /// </summary>
+    public enum Dir9
+    {
+        Center,
+        Up,
+        UpRight,
+        Right,
+        DownRight,
+        Down,
+        DownLeft,
+        Left,
+        UpLeft
+    }
+
+
+
+
 
 }
--- Runtime/Library/Utilities/GUILibrary.cs
+++ Runtime/Library/Utilities/GUILibrary.cs
@@ -5,36 +5,62 @@ namespace RealMethod
 {
     public static class RM_GUI
     {
-        public static Rect GetButtonRect(Corner corner, float width, float height, float margin = 10f)
+        public static Rect GetButtonRect(Dir9 corner, float width, float height, float margin = 10f)
         {
             float x = 0;
             float y = 0;
 
             switch (corner)
             {
-                case Corner.UpLeft:
+                case Dir9.UpLeft:
                     x = margin;
                     y = margin;
                     break;
 
-                case Corner.UpRight:
+                case Dir9.Up:
+                    x = (Screen.width - width) * 0.5f;
+                    y = margin;
+                    break;
+
+                case Dir9.UpRight:
                     x = Screen.width - width - margin;
                     y = margin;
                     break;
 
-                case Corner.DownLeft:
+                case Dir9.Left:
+                    x = margin;
+                    y = (Screen.height - height) * 0.5f;
+                    break;
+
+                case Dir9.Right:
+                    x = Screen.width - width - margin;
+                    y = (Screen.height - height) * 0.5f;
+                    break;
+
+                case Dir9.DownLeft:
                     x = margin;
                     y = Screen.height - height - margin;
                     break;
 
-                case Corner.DownRight:
+                case Dir9.Down:
+                    x = (Screen.width - width) * 0.5f;
+                    y = Screen.height - height - margin;
+                    break;
+
+                case Dir9.DownRight:
                     x = Screen.width - width - margin;
                     y = Screen.height - height - margin;
+                    break;
+
+                case Dir9.Center:
+                    x = (Screen.width - width) * 0.5f;
+                    y = (Screen.height - height) * 0.5f;
                     break;
             }
 
             return new Rect(x, y, width, height);
         }
+
     }
 
 }
