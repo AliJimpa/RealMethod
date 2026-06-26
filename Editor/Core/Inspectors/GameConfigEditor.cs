307e6b78640dd1a64eff8200cecdbcda09bf8575
Alijimpa <alibadpa007@gmail.com>
2026-06-26 17:26:42 +0330

    Implement New Version of GameConfigEditor

--- Editor/Core/Inspectors/GameConfigEditor.cs
+++ Editor/Core/Inspectors/GameConfigEditor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Reflection;
 using UnityEditor;
 
@@ -18,27 +19,24 @@ namespace RealMethod.Editor
             int totalSize = CalculateObjectSize(target);
 
             EditorGUILayout.Space();
-            EditorGUILayout.LabelField("Memory Estimate", $"{totalSize} bytes");
-            EditorGUILayout.HelpBox("Reference types (string, classes, arrays, etc.) ,only count pointer size (8 bytes on 64-bit)", MessageType.Info);
+            EditorGUILayout.HelpBox($"Memory Estimate: {totalSize} bytes", MessageType.Info);
         }
 
         private int CalculateObjectSize(object obj)
         {
             int total = 0;
-            var fields = obj.GetType().GetFields(
-                BindingFlags.Public |
-                BindingFlags.NonPublic |
-                BindingFlags.Instance);
+            var fields = obj.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 
             foreach (var field in fields)
             {
-                total += GetFieldSize(field.FieldType);
+                object value = field.GetValue(obj);
+                total += GetFieldSize(field.FieldType, value);
             }
 
             return total;
         }
 
-        private int GetFieldSize(Type type)
+        private int GetFieldSize(Type type, object value)
         {
             if (type == typeof(int)) return 4;
             if (type == typeof(float)) return 4;
@@ -49,11 +47,58 @@ namespace RealMethod.Editor
             if (type == typeof(byte)) return 1;
             if (type == typeof(char)) return 2;
 
-            // Reference types (string, classes, arrays, etc.)
-            // only count pointer size (8 bytes on 64-bit)
-            if (!type.IsValueType) return 8;
+            // String (UTF-16 in C#)
+            if (type == typeof(string))
+            {
+                string str = (string)value;
+                return str.Length * 2;
+            }
 
-            return 0;
+            // Arrays
+            if (type.IsArray)
+            {
+                int total = 0;
+                Array array = (Array)value;
+
+                foreach (var item in array)
+                    total += GetFieldSize(item.GetType(), item);
+
+                return total;
+            }
+
+            // Lists
+            if (typeof(IList).IsAssignableFrom(type))
+            {
+                int total = 0;
+                IList list = (IList)value;
+
+                foreach (var item in list)
+                    total += GetFieldSize(item.GetType(), item);
+
+                return total;
+            }
+
+            // Unity Object references → count reference only
+            if (typeof(UnityEngine.Object).IsAssignableFrom(type))
+            {
+                return IntPtr.Size;
+            }
+
+            // Custom classes/structs (recursive)
+            int objectSize = 0;
+            var Nfields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var Nfield in Nfields)
+            {
+                object Nvalue = Nfield.GetValue(value);
+                objectSize += GetFieldSize(Nfield.FieldType, Nvalue);
+            }
+            return objectSize;
+
+            // // Reference types (string, classes, arrays, etc.)
+            // // only count pointer size (8 bytes on 64-bit)
+            // if (!type.IsValueType) return 8;
+
+            // return 0;
         }
     }
 }
--- Editor/ReadySet/Tools/ClassViewerWindow.cs
+++ Editor/ReadySet/Tools/ClassViewerWindow.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Data.Common;
 using System.Reflection;
 using System.Text;
 using UnityEditor;
